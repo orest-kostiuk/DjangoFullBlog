48a44f689c4ac14f21a7fcc39c0a37d062422eba
Orest Kostiuk <xxmolekula@gmail.com>
2024-06-14 14:04:50 +0200

    added comment to post model

--- full_blog/blog/models.py
+++ full_blog/blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
 
      class Meta:
           ordering = ['-created_at',]
+
+class Comment(models.Model):
+     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+     name = models.CharField(max_length=255)
+     email = models.EmailField()
+     body = models.TextField()
+     created_at = models.DateTimeField(auto_now_add=True)
