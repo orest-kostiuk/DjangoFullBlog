9d06362c5d2f0afd25602676f1da0221d48ce047
Orest Kostiuk <xxmolekula@gmail.com>
2024-06-14 13:21:06 +0200

    added foreach posts in frontend page

--- full_blog/core/views.py
+++ full_blog/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from blog.models import Post
 
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    posts = Post.objects.all()
+    return render(request, 'core/frontpage.html', {'posts': posts})
 
 def about(request):
     return render(request, 'core/about.html')
