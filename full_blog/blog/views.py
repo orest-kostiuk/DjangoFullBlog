ab98bc547616d156ac83203f58ce357a46efdd68
Orest Kostiuk <xxmolekula@gmail.com>
2024-06-17 16:59:16 +0200

    added search on front page

--- full_blog/blog/views.py
+++ full_blog/blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render, redirect
 
 from .forms import CommentForm
@@ -27,3 +28,10 @@ def category(request, slug):
   posts = category.posts.filter(status=Post.ACTIVE)
 
   return render(request, 'blog/category.html', {'category': category, 'posts': posts})
+
+def search(request):
+  query = request.GET.get('query', '')
+
+  posts = Post.objects.filter(Q(title__icontains=query) | Q(intro__icontains=query) | Q(body__icontains=query))
+
+  return render(request, 'blog/search.html', {'posts': posts, 'query': query})
