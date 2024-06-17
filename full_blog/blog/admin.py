7f3e6fbb21a754441211c7b18223ec458ed3be1b
Orest Kostiuk <xxmolekula@gmail.com>
2024-06-17 16:38:55 +0200

    added some cool functionality to admin

--- full_blog/blog/admin.py
+++ full_blog/blog/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
-from .models import Post
-from .models import Category
+from .models import Post, Category, Comment
 
-admin.site.register(Post)
-admin.site.register(Category)
+class CommentItemInline(admin.TabularInline):
+  model = Comment
+  raw_id_fields = ['post']
+
+class PostAdmin(admin.ModelAdmin):
+  search_fields = ['title', 'intro', 'body']
+  list_display = ['title', 'slug', 'category', 'created_at']
+  list_filter = ['category', 'created_at']
+  inlines = [CommentItemInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+  search_fields = ['title']
+  list_display = ['title', 'slug']
+
+class CommentAdmin(admin.ModelAdmin):
+  search_fields = ['name', 'email', 'body']
+  list_display = ['name', 'email', 'created_at']
+
+admin.site.register(Post, PostAdmin )
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
